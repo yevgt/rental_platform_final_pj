244ccb7c744ce357607a3c029a7ac0575d8c2513
yevgt <yev.guta@gmail.com>
2025-08-25 23:30:40 +0200

    Delete notifications directory

--- notifications/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Notification
-
-class NotificationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Notification
-        fields = ["id", "type", "message", "is_read", "created_at"]
-        read_only_fields = fields
