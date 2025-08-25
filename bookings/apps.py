dd6ea5d542a53715c1cefe0a5c928bf4b261c658
yevgt <yev.guta@gmail.com>
2025-08-25 23:29:55 +0200

    Delete bookings directory

--- bookings/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class BookingsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "bookings"
-
-    def ready(self):
-        from . import signals  # noqa: F401
